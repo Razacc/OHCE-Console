78b038a56c88c9d6691314426bfdc8cbf3048c37
Razacc <165668642+Razacc@users.noreply.github.com>
2024-05-18 17:19:30 +0200

    Update miroir.py
    Ajout du cas d'utilisation pour les palindromes

--- miroir.py
+++ miroir.py
@@ -1,3 +1,6 @@
 class Miroir:
     def analyser_chaine(self, chaine: str) -> str:
         return chaine[::-1]
+        if chaine == miroir:
+            return f"{miroir} Bien dit!"
+        return miroir
