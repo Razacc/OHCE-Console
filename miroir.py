74ea6fde0c4f08269b44e034e17fb3b4d1c993a7
Razacc <165668642+Razacc@users.noreply.github.com>
2024-05-18 17:23:43 +0200

    Update miroir.py
    Ajout de "Bonjour" et "Au revoir"

--- miroir.py
+++ miroir.py
@@ -1,6 +1,8 @@
 class Miroir:
     def analyser_chaine(self, chaine: str) -> str:
         return chaine[::-1]
+        salutation = "Bonjour"
+        aurevoir = "Au revoir"
         if chaine == miroir:
-            return f"{miroir} Bien dit!"
-        return miroir
+            return f"{salutation} {miroir} Bien dit! {aurevoir}"
+        return f"{salutation} {miroir} {aurevoir}"
