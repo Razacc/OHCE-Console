5b3ce87dc51f909953541f1a31f2f8e1f9df8229
Razacc <165668642+Razacc@users.noreply.github.com>
2024-05-18 17:44:22 +0200

    Update langue.py
    Ajout des heures pour définir ce que la console doit renvoyer

--- langue.py
+++ langue.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 
 class Langue(ABC):
     @abstractmethod
-    def saluer(self) -> str:
+    def saluer(self, heure: int) -> str:
         pass
 
     @abstractmethod
@@ -10,15 +10,29 @@ class Langue(ABC):
         pass
 
     @abstractmethod
-    def acquitter(self) -> str:
+    def acquitter(self, heure: int) -> str:
         pass
 
 class LangueFrancais(Langue):
-    def saluer(self) -> str:
-        return "Bonjour"
-
+    def saluer(self, heure: int) -> str:
+        if 6 <= heure < 12:
+            return "Bonjour"
+        elif 12 <= heure < 18:
+            return "Bon après-midi"
+        elif 18 <= heure < 22:
+            return "Bonsoir"
+        else:
+            return "Bonne nuit"
+    
     def feliciter(self) -> str:
         return "Bien dit!"
-
-    def acquitter(self) -> str:
-        return "Au revoir"
+    
+    def acquitter(self, heure: int) -> str:
+        if 6 <= heure < 12:
+            return "Bonne journée"
+        elif 12 <= heure < 18:
+            return "Bon après-midi"
+        elif 18 <= heure < 22:
+            return "Bonne soirée"
+        else:
+            return "Bonne nuit"
