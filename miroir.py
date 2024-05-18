5af8b0526b4f32d0b1ae09ba4deabe35a48ff03e
Razacc <thierry.pethe@outlook.fr>
2024-05-18 18:20:24 +0200

    update langue + languages.json et suppression de message_loader

--- miroir.py
+++ miroir.py
@@ -2,7 +2,7 @@ from langue import Langue
 from horloge import Horloge
 
 class Miroir:
-    def __init__(self, langue: Langue, horloge: Horloge):
+    def init(self, langue: Langue, horloge: Horloge):
         self.langue = langue
         self.horloge = horloge
 
@@ -10,8 +10,8 @@ class Miroir:
         miroir = chaine[::-1]
         heure_actuelle = self.horloge.heure_actuelle()
         salutation = self.langue.saluer(heure_actuelle)
-
+        acquittement = self.langue.acquitter()
         if chaine == miroir:
             felicitation = self.langue.feliciter()
-            return f"{salutation} {miroir} {felicitation}"
-        return f"{salutation} {miroir}"
+            return f"{salutation} {miroir}, {felicitation} {acquittement}"
+        return f"{salutation} {miroir}, {acquittement}"
