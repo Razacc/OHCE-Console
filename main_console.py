fc30e92747bdad9ec8380b2ca7fbe7c332fdbdf8
Razacc <165668642+Razacc@users.noreply.github.com>
2024-05-18 18:35:19 +0200

    Update main_console.py

--- main_console.py
+++ main_console.py
@@ -1,15 +1,30 @@
 from miroir import Miroir
-from langue import LangueFrancais
+from langue import LangueDynamique
 from horloge import HorlogeSysteme
+from messages_loader import load_messages
 
 def main():
-    langue = LangueFrancais()
+    messages = load_messages('messages.json')
+    language_choice = input("Choose your language (English/French): ").strip().capitalize()
+    langue_messages = next((item for item in messages if item["name"] == language_choice), None)
+
+    if not langue_messages:
+        print("Invalid language choice.")
+        return
+
+    langue = LangueDynamique(langue_messages)
     horloge = HorlogeSysteme()
     miroir = Miroir(langue, horloge)
+    heure_actuelle = horloge.heure_actuelle()
+    salutation = langue.saluer(heure_actuelle)
+    print(salutation)
+    chaine = input(langue_messages["enter_text_prompt"])
     
-    chaine = input("Entrez une chaîne: ")
-    resultat = miroir.analyser_chaine(chaine)
-    print(resultat)
+    while chaine != langue_messages["exit_word"]:
+        resultat = miroir.analyser_chaine(chaine)
+        print(resultat)
+        chaine = input(langue_messages["enter_text_prompt"])
+    print(langue_messages["goodbye"])
 
 if __name__ == "__main__":
     main()
