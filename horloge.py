eb27653d2ca509ffec89e3b035803a6fbbe15d9a
paul-brd02 <paul.bardou@live.fr>
2024-05-18 20:00:33 +0200

    Update Horloge class

--- horloge.py
+++ horloge.py
@@ -1,9 +1,6 @@
 from datetime import datetime
 
 class Horloge:
-    def heure_actuelle(self) -> int:
-        pass
-
-class HorlogeSysteme(Horloge):
-    def heure_actuelle(self) -> int:
+    def heure_actuelle(self):
         return datetime.now().hour
+
